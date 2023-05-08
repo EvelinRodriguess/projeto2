86141b69688db7b6fccbe655d74ee6d5f1c33b23
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2023-05-08 10:48:18 -0300

    projeto

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue'
+const produtos = ref ()[
+
+
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90
+    }
+]
+</script>
+
+<template>
+  <ul>
+    <div v-for="item in produtos" :key="item.id"></div>
+            <p>{{ item.name }}</p>
+  </ul>
+</template>
+
+
